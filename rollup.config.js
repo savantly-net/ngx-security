3f8748f94b9268fa2a2d4d27c271a7ba4290695d
Jeremy <jdbranham@hotmail.com>
2017-09-21 02:14:55 -0500

    initial import

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from 'rollup-plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+//Add here external dependencies that actually you use.
+const globals = {
+	'@angular/core' : 'ng.core',
+	'@angular/core' : 'Injectable',
+	'@angular/common' : 'ng.common',
+	'@angular/router' : 'CanActivate',
+	'@angular/router' : 'ActivatedRouteSnapshot',
+	'@angular/router' : 'Router',
+	'rxjs/Observable' : 'Rx',
+	'rxjs/Observer' : 'Rx',
+	'rxjs/add/operator/map' : 'Rx',
+	'jwt-decode' : 'decode'
+};
+
+export default {
+	input : 'dist/index.js',
+	output : {
+		file : pkg.module,
+		format : 'umd'
+	},
+	sourcemap : true,
+	name : 'ngxSecurity',
+	plugins : [
+		resolve(),
+		commonjs(), // so Rollup can convert `ms` to an ES module
+		copy({
+			"package-publish.json" : "dist/package.json"
+		})
+	],
+	external : Object.keys(globals),
+	globals : globals
+}
