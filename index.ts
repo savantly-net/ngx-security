1a94549989978db4ec448bc1e82b6b8dd000eb4b
Jeremy Branham <jdbranham@hotmail.com>
2017-09-22 05:39:47 -0500

    ref

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { SecurityModule } from './src/security.module';
-
+export { IUser } from './src/user/user.component';
+export { IAuthenticationService } from './src/authentication/authentication.service';
