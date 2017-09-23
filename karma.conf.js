aa01137dc297081ec5f6f8d10103c005e72fd825
Jeremy Branham <jdbranham@hotmail.com>
2017-09-23 09:03:09 -0500

    updated karma to fix chrome media type issue fir ts files

--- karma.conf.js
+++ karma.conf.js
@@ -30,6 +30,10 @@ module.exports = function (config) {
 
     // list of files to exclude
     exclude: [],
+    
+    mime: {
+        'text/x-typescript': ['ts','tsx']
+	},
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
