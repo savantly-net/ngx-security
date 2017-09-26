7d82022909a73ad55ea70aa04b257261d576327a
Jeremy Branham <jdbranham@hotmail.com>
2017-09-26 01:58:28 -0500

    ref

--- index.ts
+++
@@ -1,9 +0,0 @@
-export { SecurityModule } from './src/security.module';
-export { IUser } from './src/user/user.component';
-export { IAuthenticationService } from './src/authentication/authentication.service';
-
-export { SecurityService } from './src/security.service';
-export { SecurityMockService } from './src/security-mock.service';
-export { AuthenticationService } from './src/authentication/authentication.service';
-export { AuthGaurdService } from './src/gaurds/auth-gaurd.service';
-export { RoleGaurdService } from './src/gaurds/role-gaurd.service';
